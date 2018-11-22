2cfef683277e49e7c36a83e72bf21dc99bea1a0a
裕介 鹿野 <160168@jc-21.jp>
2018-11-22 12:26:44 +0900

    完成

---
+++ BMIkadai/BMIkadai/BmiResult.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BMIkadai
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BmiResult : ContentPage
+	{
+        private double val;
+
+        public BmiResult ()
+		{
+			InitializeComponent ();
+            buttonBack.Clicked += buttonBackClicked;
+        }
+        public BmiResult(double val)
+        {
+            InitializeComponent();
+            this.val = val;
+            labelBMI.Text = "BMI=" + val;
+            buttonBack.Clicked += buttonBackClicked;
+        }
+
+        private void buttonBackClicked(object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+    }
+}
--- BMIkadai/BMIkadai/MainPage.xaml.cs
+++ BMIkadai/BMIkadai/MainPage.xaml.cs
@@ -9,9 +9,51 @@ namespace BMIkadai
 {
     public partial class MainPage : ContentPage
     {
+        double val;
+
         public MainPage()
         {
             InitializeComponent();
+            keisan.Clicked += keisanClicked;
+            keisan2.Clicked += keisanClicked2;
         }
+
+        private void keisanClicked(object sender, EventArgs e)
+        {
+            float s = float.Parse(sin.Text);
+            int t = int.Parse(tai.Text);
+            if (s > 0 && s <= 3.0)
+            {
+                val = (t / s / s);
+               
+            }
+            else if(s > 10 && s < 300)
+            {
+                val = (t / ((s / 100) * (s / 100)));
+            }
+            val = Math.Round(val, 1, MidpointRounding.AwayFromZero);
+            string Val = val.ToString();
+            bmi.Text = Val;
+        }
+
+        private void keisanClicked2(object sender, EventArgs e)
+        {
+            float s = float.Parse(sin.Text);
+            int t = int.Parse(tai.Text);
+            if (s > 0 && s <= 3.0)
+            {
+                val = (t / s / s);
+
+            }
+            else if (s > 10 && s < 300)
+            {
+                val = (t / ((s / 100) * (s / 100)));
+            }
+            val = Math.Round(val, 1, MidpointRounding.AwayFromZero);
+            string Val = val.ToString();
+            Navigation.PushModalAsync(new NavigationPage(new BmiResult(val)));
+        }
+
     }
+
 }
